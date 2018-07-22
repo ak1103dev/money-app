31bc4444be7fe484346b039503b9f91e9501dbd6
ak1103dev <ak.apichan1103@gmail.com>
2018-07-22 12:19:37 +0700

    :tada: init fontend

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import './index.css';
+import App from './ExchangeRates';
+import registerServiceWorker from './registerServiceWorker';
+
+const client = new ApolloClient({
+  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
