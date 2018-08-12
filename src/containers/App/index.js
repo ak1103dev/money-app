84635f9912b76fdd78c1fddc0e861c3fbc76c33b
ak1103dev <ak.apichan1103@gmail.com>
2018-08-12 22:57:44 +0700

    :art: add route

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import DashboardPage from '../DashboardPage';
+import HomePage from '../HomePage';
 
 class App extends Component {
   constructor() {
@@ -7,7 +10,18 @@ class App extends Component {
   }
 
   render() {
-    return <div>My App {'hello'}</div>;
+    return (
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/dashboard" component={DashboardPage} />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const client = new ApolloClient({
-  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql'
 });
 
 ReactDOM.render(
